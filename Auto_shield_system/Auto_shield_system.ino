74271fc1cc5f5d7e6979dd055810f2a714a734c8
Thisaru Dissanayake <120794441+thisaru500@users.noreply.github.com>
2024-10-18 21:58:08 +0530

    add cording part for components

--- Auto_shield_system/Auto_shield_system.ino
+++ Auto_shield_system/Auto_shield_system.ino
@@ -3,17 +3,27 @@
 Servo yakada; 
 SoftwareSerial sim900(7, 8);  // RX, TX
 
-void setup() {
+   int smoke_sensor =A0;
+   int safety_lim = 60;
+   int ledset = 12;
+   int relay = 10;
    int servo = 9;
+   int flame = 11;
+   int pos = 0;
+   int buzzer = 3;
+   
+void setup() {
+   
   yakada.attach(servo);
   Serial.begin(9600);  
   sim900.begin(9600);  
   delay(1000);
 
-   int smoke_sensor =A0;
-   int safety_lim = 60;
-   int ledset = 9;
-   int relay = 10;
+  pinMode(smoke_sensor , INPUT);
+  pinMode(ledset , OUTPUT);
+  pinMode(relay , OUTPUT);
+  pinMode(flame , INPUT);
+  pinMode(buzzer , OUTPUT);
    
   
 }
@@ -40,9 +50,33 @@ void Sim900a() {
   Serial.println("Message sent.");
 }
 
+void servo1() {
+  for (pos = 0; pos <= 90; pos += 1) { 
+  
+    yakada.write(pos);             
+    delay(20);                    
+  }
+
 void loop() {
    
-  
+ int sensor_read =  analogRead(smoke_sensor);
+ int flame_value = digitalRead(flame);
+
+ if(sensor_read >= safety_lim || flame_value == 1)
+ { 
+   sim900a();
+   servo1();
+   digitalWrite(relay , HIGH);//for turn off main switch
+   for(int i = 50; i>0 ; i--){
+
+    digitalWrite(buzzer , HIGH);
+    delay(1000);
+   }
+   
+
+   
+
+ 
 
   
 }
